9630ab0808a0d898bf2e4a6fb57c7f109787d216
Leonid Kostrykin <void@evoid.de>
2023-12-05 18:43:04 +0000

    Fix problems

--- activities/cli.py
+++ activities/cli.py
@@ -18,7 +18,13 @@ if __name__ == '__main__':
     for repository_url in repositories:
         if 'bmcv' not in repository_url.lower(): continue
         repo = fetch.get_github_repository(g, repository_url)
-        for c in fetch.get_commit_history(repo):
-            if c.author is None: continue
-            print(c.author.login, c.last_modified)
-        break
+        #for c in repo.get_commits():
+        #    if c.author is None: continue
+        #    print(c.author.login, c.last_modified)
+        #    file = c.files[0]
+        #    print(file.contents_url, file.blob_url)
+        #    print('---')
+        #    print(fetch.get_string_content(repo.get_contents(file.filename, ref=c.sha)))
+        #    break
+        #break
+        fetch.get_commit_history(repo)
--- activities/fetch.py
+++ activities/fetch.py
@@ -1,5 +1,6 @@
 import base64
 import re
+import pathlib
 from typing import (
     Any,
     Dict,
@@ -8,13 +9,19 @@ from typing import (
 )
 
 import pandas as pd
+import yaml
+
+from tqdm import tqdm
 
 from github import Github
 from github.ContentFile import ContentFile
 from github.Repository import Repository
+from github.Commit import Commit
+from github.GithubException import UnknownObjectException
 
 
 GITHUB_URL = 'https://github.com/'
+SHED_FILENAME = '.shed.yml'
 
 
 def get_string_content(cf: ContentFile) -> str:
@@ -39,7 +46,7 @@ def get_tool_github_repositories(g: Github) -> List[str]:
     repo = g.get_user('galaxyproject').get_repo('planemo-monitor')
     repo_list: List[str] = []
     for i in range(1, 5):
-        repo_f = repo.get_contents(f'repositories0{i}.list')
+        repo_f = repo.get_contents(f'repositories{i:02d}.list')
         repo_l = get_string_content(repo_f).rstrip()
         repo_list.extend(repo_l.split('\n'))
     return repo_list
@@ -56,23 +63,49 @@ def get_github_repository(g: Github, repository_url: str) -> Repository:
 
 
 def get_cached_commit_history(repository: Repository) -> pd.DataFrame:
-    return pd.DataFrame(columns=['author', 'timestamp'])
+    return pd.DataFrame(columns=['author', 'timestamp', 'categories'])
 
 
 def set_cached_commit_history(repository: Repository, history: pd.DataFrame):
     pass
 
 
+def is_subpath(subpath: pathlib.Path, path: pathlib.Path) -> bool:
+    return str(subpath) in [str(path)] + [str(p) for p in path.parents[:-1]]
+
+
+def get_updated_tool_categories(repository: Repository, commit: Commit, pbar: tqdm) -> list:
+    updated_tool_categories = set()
+    tool_directories = list()
+    for file in commit.files:
+        updated_directories = pathlib.Path(file.filename).parents[:-1]
+        if any([is_subpath(tool_directory, updated_directories[0]) for tool_directory in tool_directories]): continue
+        for updated_directory in updated_directories:
+            try:
+                shed_filepath = str(updated_directory / SHED_FILENAME)
+                pbar.set_description(f'Peeking {shed_filepath}')
+                shed_file = get_string_content(repository.get_contents(shed_filepath, ref=commit.sha))
+                shed_data = yaml.safe_load(shed_file)
+                updated_tool_categories |= set(shed_data['categories'])
+                tool_directories.append(updated_directory)
+                break
+            except UnknownObjectException:
+                pass
+    return list(updated_tool_categories)
+
+
 def get_commit_history(repository: Repository) -> pd.DataFrame:
     cached_df = get_cached_commit_history(repository)
     last_cache_update = pd.to_datetime(0)
-    new_entries = dict(author=list(), timestamp=list())
-    for c in repository.get_commits():
+    new_entries = dict(author=list(), timestamp=list(), directory=list(), categories=list())
+    for c in (pbar := tqdm(repository.get_commits(), total=repository.get_commits().totalCount)):
         if c.author is None: continue
         datetime = pd.to_datetime(c.last_modified)
         if datetime <= last_cache_update: break
+        updated_tool_categories = get_updated_tool_categories(repository, c, pbar)
         new_entries['author'].append(c.author.login)
-        new_entries['timestamps'].append(datetime)
+        new_entries['timestamp'].append(datetime)
+        new_entries['categories'].append(','.join(updated_tool_categories))
     new_entries_df = pd.DataFrame(new_entries)
     history_df = pd.concat([cached_df, new_entries_df])
     set_cached_commit_history(repository, history_df)
