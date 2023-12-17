d647afd03b1ed45b48202f214c37301ef483a60a
Leonid Kostrykin <void@evoid.de>
2023-12-17 19:48:38 +0100

    WIP

--- activities/report.py
+++ activities/report.py
@@ -1,8 +1,55 @@
 import os
+import re
+import glob
+import csv
+import urllib.request
 
 import pandas as pd
 import yaml
 from liquid import Template
+
+
+def get_community_dataframe(community):
+
+    # Get list of repositories relevant to the community
+    if 'repositories' in community:
+        repositories = community['repositories']
+    else:
+        repositories = list()
+        for cache_filepath in glob.glob('cache/*/*.csv'):
+            match = re.match(r'^cache/(.*).csv$', cache_filepath)
+            repositories.append(match.group(1))
+
+    # Get list of tool categories relevant to the community (if any)
+    if 'categories' in community:
+        categories = list()
+        for cinfo in community['categories']:
+            if 'expand' in cinfo:
+                for category in urllib.request.urlopen(cinfo['expand']):
+                    category = category.decode('utf-8').strip()
+                    categories.append(category)
+            else:
+                assert isinstance(cinfo, str), str(cinfo)
+                categories.append(cinfo)
+                
+    else:
+        categories = None
+
+    # Read the repositories and keep only the rows with matching categories
+    df_list = list()
+    for repo in repositories:
+        df = pd.read_csv(f'cache/{repo}.csv')
+        df.categories = df.categories.fillna('')
+        if categories is not None:
+            drop_idx_list = list()
+            for row_idx, row in enumerate(df['categories']):
+                row_categories = [c.lower().strip() for c in row.split(',')]
+                if not any([c.lower() in row_categories for c in categories]):
+                    drop_idx_list.append(row_idx)
+            df.drop(drop_idx_list, inplace=True)
+            df['repository'] = repo
+        df_list.append(df)
+    return pd.concat(df_list)
 
 
 def update_communities():
@@ -18,6 +65,10 @@ def update_communities():
     # Render community pages
     os.makedirs('report/communities', exist_ok=True)
     for community in communities:
+        df = get_community_dataframe(community)
+        communities_data_dir = 'report/_data/communities_data'
+        os.makedirs(communities_data_dir, exist_ok=True)
+        df.to_csv(f'{communities_data_dir}/{community["id"]}.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
         with open(f'report/communities/{community["id"]}.md', 'w') as fp:
             fp.write(template.render(community = community))
 
--- report/_community.md
+++ report/_community.md
@@ -1,10 +1,25 @@
 ---
 title: {{ community.name }}
 layout: default
+community_id: {{ community.id }}
 ---
 
-{% raw %}<h1 class="mb-5">x {{ page.title }}</h1>{% endraw %}
+{% raw %}
 
-**Bla:**
 
-asdf
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="../index.html">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page">{{ page.title }}</li>
+  </ol>
+</nav>
+
+<h1 class="mb-5">{{ page.title }}</h1>
+
+{% assign community_data = site.data.communities_data[page.community_id] %}
+
+<h2>Community Overview</h2>
+
+**Commits:** {{ community_data.size }}
+
+{% endraw %}
